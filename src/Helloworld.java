86540667ecbde11dbebe7f16e86917d1fa37e8d7
3dhanushkrishna <3dhanushkccd@gmail.com>
2022-03-14 12:27:52 +0530

    java programs

---
+++ src/Helloworld.java
@@ -0,0 +1,13 @@
+public class Helloworld {
+    public static void main(String[] args) {
+        Integer a=20;
+        if (a%2 == 0){
+            System.out.println("it's even number");
+
+        }
+        else{
+            System.out.println("it's odd number");
+        }
+
+    }
+}
